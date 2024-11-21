da5cdcecf2af8e426cdc1fe88afdd25482f6581d
MoisesRangel 7502410027 <mrangell1@unicartagena.edu.co>
2024-11-21 11:36:40 -0500

    Add files via upload

---
+++ sistema mensajero.java
@@ -0,0 +1,36 @@
+public class Mensajero {
+        public void enviarMensaje(String mensaje, String destino) {
+            System.out.println("Enviando correo electrónico a");
+            System.out.println("Mensaje");
+        }
+    }
+    public interface CanalDeComunicacion {
+        void enviarMensaje(String mensaje, String destino);
+    }
+    public class CorreoElectronico implements CanalDeComunicacion {
+        public void enviarMensaje(String mensaje, String destino) {
+            System.out.println("Enviando correo electrónico a: " + destino);
+            System.out.println("Mensaje: " + mensaje);
+        }
+    }
+    public class SMS implements CanalDeComunicacion {
+        public void enviarMensaje(String mensaje, String destino) {
+            System.out.println("Enviando SMS a: " + destino);
+            System.out.println("Mensaje: " + mensaje);
+        }
+    }
+    public class NotificacionPush implements CanalDeComunicacion {
+        public void enviarMensaje(String mensaje, String destino) {
+            System.out.println("Enviando notificación push a: " + destino);
+            System.out.println("Mensaje: " + mensaje);
+        }
+    }
+    public class Mensajero {
+        - CanalDeComunicacion canal;
+        public Mensajero(CanalDeComunicacion canal) {
+            this.canal = canal;
+        }
+        public void enviarMensaje(String mensaje, String destino) {
+            canal.enviarMensaje(mensaje, destino);
+        }
+    }
